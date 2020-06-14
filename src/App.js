f7732304331943c4ebc83207a047b1bfd661a3db
Red Cap Tom <hey@redcaptom.com>
2020-06-14 23:52:15 +0300

    Post 12 - User Interface 2 (https://dev.to/redcaptom/shopify-app-from-scratch-12-user-interface-2-3327)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { Page, AppProvider, Layout, Card } from "@shopify/polaris";
+import { Page, AppProvider, Layout, Card, RadioButton } from "@shopify/polaris";
 import "@shopify/polaris/styles.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonType: "empty",
+    };
+  }
+
   render() {
+    const handleButtonTypeSelection = (changedButtonType) => {
+      this.setState({ buttonType: changedButtonType });
+    };
     return (
       <AppProvider>
         <Page
@@ -11,17 +21,37 @@ class App extends React.Component {
           subtitle="Please select the type of button you'd like to generate for your site:"
         >
           <Layout>
-            <Layout.Section oneHalf>
-              <Card title="Black Button Variant">
+            <Layout.Section oneHalf secondary>
+              <Card title="Black Button Variant" sectioned>
                 <Card.Section title="Demo">
-                  <button>Dummy Black Button</button>
+                  <button>Dummy Full Button</button>
+                </Card.Section>
+                <Card.Section>
+                  <RadioButton
+                    label="Choose Full Button"
+                    helpText="Works well with the default Shopify themes and lighter backgrounds."
+                    checked={this.state.buttonType === "full"}
+                    id="full"
+                    name="full-button"
+                    onChange={() => handleButtonTypeSelection("full")}
+                  />
                 </Card.Section>
               </Card>
             </Layout.Section>
             <Layout.Section oneHalf>
-              <Card title="White Button Variant">
+              <Card title="Empty Button Variant" sectioned>
                 <Card.Section title="Demo">
-                  <button>Dummy White Button</button>
+                  <button>Dummy Empty Button</button>
+                </Card.Section>
+                <Card.Section>
+                  <RadioButton
+                    label="Choose Empty Button"
+                    helpText="Works well with darker backgrounds, to create a high-contrast feel."
+                    checked={this.state.buttonType === "empty"}
+                    id="empty"
+                    name="empty-button"
+                    onChange={() => handleButtonTypeSelection("empty")}
+                  />
                 </Card.Section>
               </Card>
             </Layout.Section>
